655dc167aa0914326818cb49b5f37f25a3b6b2b7
IwoSzczepaniak <iszczepaniak@student.agh.edu.pl>
2023-01-24 20:50:18 +0100

    prs

--- prs.R
+++ prs.R
@@ -1,14 +1,26 @@
-prs2d <- function(func, lbound, upbound) {
+library(smoof)
+library("GA")
+
+ackley2d <- makeAckleyFunction(c(2))
+ackley10d <- makeAckleyFunction(c(10))
+ackley20d <- makeAckleyFunction(c(20))
+
+
+prs <- function(func, lbound, upbound, dims) {
   best = Inf
   for (i in (1:1000)) {
-    v <- runif(2, lbound, upbound)
+    v <- runif(dims, lbound, upbound)
     current <- func(v)
     if (current < best) {
       best <- current
     }
   }
   return (best)
-}
+};
 
-result <- replicate(100, prs2d(ackley2d, c(1, 1), c(100, 100)))
+
+result <- replicate(100, prs(ackley10d, rep(1, 10), rep(100, 10), 10))
+result
 mean(result)
+
+
