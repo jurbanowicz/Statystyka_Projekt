fd00c29483ef808a3a908567287d1ff39aac430d
jurbanowicz <jurbanowicz@student.agh.edu.pl>
2023-01-26 12:28:29 +0100

    added markdown

--- main.R
+++ main.R
@@ -14,7 +14,7 @@ prs <- function(func, lbound, upbound, dims) {
 };
 
 ga_result <- function(func, lbound, ubound){
-  result <- ga(type = "real-valued", fitness =function(x) - func(x), maxiter = 100, monitor = FALSE, lower = c(th = lbound), upper = ubound)
+  result <- ga(type = "real-valued", fitness =function(x) - func(x), maxiter = 1000, monitor = FALSE, lower = c(th = lbound), upper = ubound)
   return(result@fitnessValue*(-1))
 };
 
@@ -46,6 +46,46 @@ ras2d_ga <- replicate(50, ga_result(rastrigin2d, rep(-5, 2), rep(5, 2)))
 ras10d_ga <- replicate(50, ga_result(rastrigin10d, rep(-5, 10), rep(5, 10)))
 ras20d_ga <- replicate(50, ga_result(rastrigin20d, rep(-5, 20), rep(5, 20)))
 
+# Boxplots and histograms for each of teh results
+run_comparison <- function() {
+hist(ack2d_prs, main = paste("Ackley histogram for ", 2, " dimensions: prs"))
+boxplot(ack2d_prs, main = paste("Ackley boxplot for ", 2, " dimensions: prs"))
+
+hist(ack2d_ga, main = paste("Ackley histogram for ", 2, " dimensions: ga"))
+boxplot(ack2d_ga, main = paste("Ackley boxplot for ", 2, " dimensions: ga"))
+
+hist(ack10d_prs, main = paste("Ackley histogram for ", 10, " dimensions: prs"))
+boxplot(ack10d_prs, main = paste("Ackley boxplot for ", 10, " dimensions: prs"))
+
+hist(ack10d_ga, main = paste("Ackleyhistogram for ", 10, " dimensions: ga"))
+boxplot(ack10d_ga, main = paste("Ackley boxplot for ", 10, " dimensions: ga"))
+
+hist(ack20d_prs, main = paste("Ackley histogram for ", 20, " dimensions: prs"))
+boxplot(ack20d_prs, main = paste("Ackley boxplot for ", 20, " dimensions: prs"))
+
+hist(ack20d_ga, main = paste("Ackley histogram for ", 20, " dimensions: ga"))
+boxplot(ack20d_ga, main = paste("Ackley boxplot for ", 20, " dimensions using ga"))
+
+hist(ras2d_prs, main = paste("Rastrigin histogram for ", 2, " dimensions: prs"))
+boxplot(ras2d_prs, main = paste("Rastrigin boxplot for ", 2, " dimensions: prs"))
+
+hist(ras2d_ga, main = paste("Rastrigin histogram for ", 2, " dimensions: ga"))
+boxplot(ras2d_ga, main = paste("Rastrigin boxplot for ", 2, " dimensions: ga"))
+
+hist(ras10d_prs, main = paste("Rastrigin histogram for ", 10, " dimensions: prs"))
+boxplot(ras10d_prs, main = paste("Rastrigin boxplot for ", 10, " dimensions: prs"))
+
+hist(ras10d_ga, main = paste("Rastrigin histogram for ", 10, " dimensions: ga"))
+boxplot(ras10d_ga, main = paste("Rastrigin boxplot for ", 10, " dimensions: ga"))
+
+hist(ras20d_prs, main = paste("Rastrigin histogram for ", 20, " dimensions: prs"))
+boxplot(ras20d_prs, main = paste("Rastrigin boxplot for ", 20, " dimensions: prs"))
+
+hist(ras20d_ga, main = paste("Rastrigin histogram for ", 20, " dimensions: ga"))
+boxplot(ras20d_ga, main = paste("Rastrigin boxplot for ", 20, " dimensions: ga"))
+}
+
+
 # comparison between each of the optimalization method
 t.test(ack2d_prs, ack2d_ga)
 t.test(ack10d_prs, ack10d_ga)
@@ -60,6 +100,11 @@ library(ggplot2)
 library(tibble)
 
 data <- data_frame(method = c(rep("PRS2D",length(ack2d_prs)),rep("PRS10D",length(ack10d_prs))),value = c(ack2d_prs,ack10d_prs))
+ggplot(data=data, aes(x= method, y=value)) +
+  geom_boxplot()+
+  ggtitle("Ackley")
+
+data <- data_frame(method = c(rep("PRS2D",length(ack2d_prs)),rep("GA2D",length(ack2d_ga))),value = c(ack2d_prs,ack2d_ga))
 ggplot(data=data, aes(x= method, y=value)) +
   geom_boxplot()+
   ggtitle("Ackley")
